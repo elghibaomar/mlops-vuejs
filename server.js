0cf52271267fbacf94ede18e62f8264872ff91fe
elghibaomar <elghibaomar@gmail.com>
2022-07-20 15:23:16 +0100

    h5

--- server.js
+++ server.js
@@ -4,5 +4,5 @@ var serveStatic = require('serve-static');
 var app = express();
 app.use(serveStatic(__dirname + "/dist"));
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8082;
 app.listen(port);
